240b9c49d5d82805f8a5754748a51898e7ec4095
mistachkin <mistachkin>
2015-01-20 15:45:22 +0000

    Make it easier to test a subset of the supported Visual Studio versions.

--- Setup/build_ce_200x.bat
+++ Setup/build_ce_200x.bat
@@ -30,10 +30,48 @@ SET TOOLS=%TOOLS:~0,-1%
 
 %_VECHO% Tools = '%TOOLS%'
 
-SET BUILD_CONFIGURATIONS=Debug Release
+CALL :fn_ResetErrorLevel
+
+%__ECHO3% CALL "%TOOLS%\vsSp.bat"
+
+IF ERRORLEVEL 1 (
+  ECHO Could not detect Visual Studio.
+  GOTO errors
+)
+
+IF DEFINED BUILD_DEBUG (
+  SET BUILD_CONFIGURATIONS=Debug Release
+) ELSE (
+  SET BUILD_CONFIGURATIONS=Release
+)
+
 SET BASE_CONFIGURATIONSUFFIX=Compact
 SET PLATFORMS="Pocket PC 2003 (ARMV4)"
-SET YEARS=2005 2008
+
+REM
+REM NOTE: The .NET Compact Framework is only supported by Visual Studio 2005
+REM       and 2008, regardless of which versions of Visual Studio are installed
+REM       on this machine; therefore, override the YEARS variable limiting it
+REM       to 2005 and 2008 only.
+REM
+CALL :fn_UnsetVariable YEARS
+
+IF NOT DEFINED NOVS2005 (
+  IF DEFINED VS2005SP (
+    CALL :fn_AppendVariable YEARS " 2005"
+  )
+)
+
+IF NOT DEFINED NOVS2008 (
+  IF DEFINED VS2008SP (
+    CALL :fn_AppendVariable YEARS " 2008"
+  )
+)
+
+%_VECHO% BuildConfigurations = '%BUILD_CONFIGURATIONS%'
+%_VECHO% BaseConfigurationSuffix = '%BASE_CONFIGURATIONSUFFIX%'
+%_VECHO% Platforms = '%PLATFORMS%'
+%_VECHO% Years = '%YEARS%'
 
 CALL :fn_ResetErrorLevel
 
@@ -43,6 +81,26 @@ IF ERRORLEVEL 1 (
   ECHO Failed to build WinCE binaries.
   GOTO errors
 )
+
+:fn_AppendVariable
+  SET __ECHO_CMD=ECHO %%%1%%
+  IF DEFINED %1 (
+    FOR /F "delims=" %%V IN ('%__ECHO_CMD%') DO (
+      SET %1=%%V%~2
+    )
+  ) ELSE (
+    SET %1=%~2
+  )
+  SET __ECHO_CMD=
+  CALL :fn_ResetErrorLevel
+  GOTO :EOF
+
+:fn_UnsetVariable
+  IF NOT "%1" == "" (
+    SET %1=
+    CALL :fn_ResetErrorLevel
+  )
+  GOTO :EOF
 
 :fn_ResetErrorLevel
   VERIFY > NUL
