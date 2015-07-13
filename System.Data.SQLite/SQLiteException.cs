ce81771cff05b9b2c3931968d7349eb8a0fdeeda
mistachkin <mistachkin>
2015-07-13 19:51:09 +0000

    Add extended return codes to the SQLiteErrorCode enumeration. Pursuant to [71bedaca19].

--- System.Data.SQLite/SQLiteException.cs
+++ System.Data.SQLite/SQLiteException.cs
@@ -332,6 +332,223 @@ namespace System.Data.SQLite
     /// <summary>
     /// Used to mask off extended result codes
     /// </summary>
-    NonExtendedMask = 0xFF
+    NonExtendedMask = 0xFF,
+
+    ///////////////////////////////////////////////////////////////////////////
+    // BEGIN EXTENDED RESULT CODES
+    ///////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// A file read operation failed.
+    /// </summary>
+    IoErr_Read = (IoErr | (1 << 8)),
+    /// <summary>
+    /// A file read operation returned less data than requested.
+    /// </summary>
+    IoErr_Short_Read = (IoErr | (2 << 8)),
+    /// <summary>
+    /// A file write operation failed.
+    /// </summary>
+    IoErr_Write = (IoErr | (3 << 8)),
+    /// <summary>
+    /// A file synchronization operation failed.
+    /// </summary>
+    IoErr_Fsync = (IoErr | (4 << 8)),
+    /// <summary>
+    /// A directory synchronization operation failed.
+    /// </summary>
+    IoErr_Dir_Fsync = (IoErr | (5 << 8)),
+    /// <summary>
+    /// A file truncate operation failed.
+    /// </summary>
+    IoErr_Truncate = (IoErr | (6 << 8)),
+    /// <summary>
+    /// A file metadata operation failed.
+    /// </summary>
+    IoErr_Fstat = (IoErr | (7 << 8)),
+    /// <summary>
+    /// A file unlock operation failed.
+    /// </summary>
+    IoErr_Unlock = (IoErr | (8 << 8)),
+    /// <summary>
+    /// A file lock operation failed.
+    /// </summary>
+    IoErr_RdLock = (IoErr | (9 << 8)),
+    /// <summary>
+    /// A file delete operation failed.
+    /// </summary>
+    IoErr_Delete = (IoErr | (10 << 8)),
+    /// <summary>
+    /// Not currently used.
+    /// </summary>
+    IoErr_Blocked = (IoErr | (11 << 8)),
+    /// <summary>
+    /// Out-of-memory during a file operation.
+    /// </summary>
+    IoErr_NoMem = (IoErr | (12 << 8)),
+    /// <summary>
+    /// A file existence/status operation failed.
+    /// </summary>
+    IoErr_Access = (IoErr | (13 << 8)),
+    /// <summary>
+    /// A check for a reserved lock failed.
+    /// </summary>
+    IoErr_CheckReservedLock = (IoErr | (14 << 8)),
+    /// <summary>
+    /// A file lock operation failed.
+    /// </summary>
+    IoErr_Lock = (IoErr | (15 << 8)),
+    /// <summary>
+    /// A file close operation failed.
+    /// </summary>
+    IoErr_Close = (IoErr | (16 << 8)),
+    /// <summary>
+    /// A directory close operation failed.
+    /// </summary>
+    IoErr_Dir_Close = (IoErr | (17 << 8)),
+    /// <summary>
+    /// A shared memory open operation failed.
+    /// </summary>
+    IoErr_ShmOpen = (IoErr | (18 << 8)),
+    /// <summary>
+    /// A shared memory size operation failed.
+    /// </summary>
+    IoErr_ShmSize = (IoErr | (19 << 8)),
+    /// <summary>
+    /// A shared memory lock operation failed.
+    /// </summary>
+    IoErr_ShmLock = (IoErr | (20 << 8)),
+    /// <summary>
+    /// A shared memory map operation failed.
+    /// </summary>
+    IoErr_ShmMap = (IoErr | (21 << 8)),
+    /// <summary>
+    /// A file seek operation failed.
+    /// </summary>
+    IoErr_Seek = (IoErr | (22 << 8)),
+    /// <summary>
+    /// A file delete operation failed because it does not exist.
+    /// </summary>
+    IoErr_Delete_NoEnt = (IoErr | (23 << 8)),
+    /// <summary>
+    /// A file memory mapping operation failed.
+    /// </summary>
+    IoErr_Mmap = (IoErr | (24 << 8)),
+    /// <summary>
+    /// The temporary directory path could not be obtained.
+    /// </summary>
+    IoErr_GetTempPath = (IoErr | (25 << 8)),
+    /// <summary>
+    /// A path string conversion operation failed.
+    /// </summary>
+    IoErr_ConvPath = (IoErr | (26 << 8)),
+    /// <summary>
+    /// A database table is locked in shared-cache mode.
+    /// </summary>
+    Locked_SharedCache = (Locked | (1 << 8)),
+    /// <summary>
+    /// A database file is locked due to a recovery operation.
+    /// </summary>
+    Busy_Recovery = (Busy | (1 << 8)),
+    /// <summary>
+    /// A database file is locked due to snapshot semantics.
+    /// </summary>
+    Busy_Snapshot = (Busy | (2 << 8)),
+    /// <summary>
+    /// A database file cannot be opened because no temporary directory is available.
+    /// </summary>
+    CantOpen_NoTempDir = (CantOpen | (1 << 8)),
+    /// <summary>
+    /// A database file cannot be opened because its path represents a directory.
+    /// </summary>
+    CantOpen_IsDir = (CantOpen | (2 << 8)),
+    /// <summary>
+    /// A database file cannot be opened because its full path could not be obtained.
+    /// </summary>
+    CantOpen_FullPath = (CantOpen | (3 << 8)),
+    /// <summary>
+    /// A database file cannot be opened because a path string conversion operation failed.
+    /// </summary>
+    CantOpen_ConvPath = (CantOpen | (4 << 8)),
+    /// <summary>
+    /// A virtual table is malformed.
+    /// </summary>
+    Corrupt_Vtab = (Corrupt | (1 << 8)),
+    /// <summary>
+    /// A database file is read-only due to a recovery operation.
+    /// </summary>
+    ReadOnly_Recovery = (ReadOnly | (1 << 8)),
+    /// <summary>
+    /// A database file is read-only because a lock could not be obtained.
+    /// </summary>
+    ReadOnly_CantLock = (ReadOnly | (2 << 8)),
+    /// <summary>
+    /// A database file is read-only because it needs rollback processing.
+    /// </summary>
+    ReadOnly_Rollback = (ReadOnly | (3 << 8)),
+    /// <summary>
+    /// A database file is read-only because it was moved while open.
+    /// </summary>
+    ReadOnly_DbMoved = (ReadOnly | (4 << 8)),
+    /// <summary>
+    /// An operation is being aborted due to rollback processing.
+    /// </summary>
+    Abort_Rollback = (Abort | (2 << 8)),
+    /// <summary>
+    /// A CHECK constraint failed.
+    /// </summary>
+    Constraint_Check = (Constraint | (1 << 8)),
+    /// <summary>
+    /// A commit hook produced a unsuccessful return code.
+    /// </summary>
+    Constraint_CommitHook = (Constraint | (2 << 8)),
+    /// <summary>
+    /// A FOREIGN KEY constraint failed.
+    /// </summary>
+    Constraint_ForeignKey = (Constraint | (3 << 8)),
+    /// <summary>
+    /// Not currently used.
+    /// </summary>
+    Constraint_Function = (Constraint | (4 << 8)),
+    /// <summary>
+    /// A NOT NULL constraint failed.
+    /// </summary>
+    Constraint_NotNull = (Constraint | (5 << 8)),
+    /// <summary>
+    /// A PRIMARY KEY constraint failed.
+    /// </summary>
+    Constraint_PrimaryKey = (Constraint | (6 << 8)),
+    /// <summary>
+    /// The RAISE function was used by a trigger-program.
+    /// </summary>
+    Constraint_Trigger = (Constraint | (7 << 8)),
+    /// <summary>
+    /// A UNIQUE constraint failed.
+    /// </summary>
+    Constraint_Unique = (Constraint | (8 << 8)),
+    /// <summary>
+    /// Not currently used.
+    /// </summary>
+    Constraint_Vtab = (Constraint | (9 << 8)),
+    /// <summary>
+    /// A ROWID constraint failed.
+    /// </summary>
+    Constraint_RowId = (Constraint | (10 << 8)),
+    /// <summary>
+    /// Frames were recovered from the WAL log file.
+    /// </summary>
+    Notice_Recover_Wal = (Notice | (1 << 8)),
+    /// <summary>
+    /// Pages were recovered from the journal file.
+    /// </summary>
+    Notice_Recover_Rollback = (Notice | (2 << 8)),
+    /// <summary>
+    /// An automatic index was created to process a query.
+    /// </summary>
+    Warning_AutoIndex = (Warning | (1 << 8)),
+    /// <summary>
+    /// User authentication failed.
+    /// </summary>
+    Auth_User = (Auth | (1 << 8))
   }
 }
