110c921fd6155470de1e499bb97c64d980a66bf1
mistachkin <mistachkin>
2016-03-24 23:10:35 +0000

    Add better detection of the right shared library name for the POSIX sub-platform.

--- Setup/run-mono-tests-debug.sh
+++ Setup/run-mono-tests-debug.sh
@@ -2,6 +2,12 @@
 
 scriptdir=`dirname "$BASH_SOURCE"`
 
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  libname=libsqlite3.dylib
+else
+  libname=libsqlite3.so.0
+fi
+
 pushd "$scriptdir/.."
-mono Externals/Eagle/bin/EagleShell.exe -preInitialize "set test_configuration Debug; set build_directory {bin/2013/Debug/bin}; set native_library_file_names libsqlite3.so.0" -file Tests/all.eagle
+mono Externals/Eagle/bin/EagleShell.exe -preInitialize "set test_configuration Debug; set build_directory {bin/2013/Debug/bin}; set native_library_file_names $libname" -file Tests/all.eagle
 popd
