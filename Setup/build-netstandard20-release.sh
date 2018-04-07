760527222860f04af9bb606950d4b4f8f099c9dc
mistachkin <mistachkin>
2018-04-07 19:10:58 +0000

    Use 'SQLITE_NET_CONFIGURATION_SUFFIX' in the .NET Core build scripts for POSIX.

--- Setup/build-netstandard20-release.sh
+++ Setup/build-netstandard20-release.sh
@@ -3,6 +3,6 @@
 scriptdir=`dirname "$BASH_SOURCE"`
 pushd "$scriptdir/.."
 
-dotnet build SQLite.NET.NetStandard20.MSBuild.sln /property:Configuration=Release /property:ConfigurationSuffix=NetStandard20 /property:InteropCodec=false /property:InteropLog=false "$@"
+dotnet build SQLite.NET.NetStandard20.MSBuild.sln /property:Configuration=Release /property:ConfigurationSuffix=$SQLITE_NET_CONFIGURATION_SUFFIX /property:InteropCodec=false /property:InteropLog=false "$@"
 
 popd
