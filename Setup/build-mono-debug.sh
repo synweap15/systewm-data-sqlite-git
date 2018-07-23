fbd0f4edb3bf22eb35689ca564dfa888b7aa0dd4
mistachkin <mistachkin>
2018-07-23 06:19:00 +0000

    Enhancements to POSIX build scripts.

---
+++ Setup/build-mono-debug.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+scriptdir=`dirname "$BASH_SOURCE"`
+
+if [[ -z "$SQLITE_NET_YEAR" ]]; then
+  SQLITE_NET_YEAR=2013
+fi
+
+pushd "$scriptdir/.."
+xbuild SQLite.NET.$SQLITE_NET_YEAR.MSBuild.sln /property:Configuration=Debug /property:ConfigurationSuffix=$SQLITE_NET_CONFIGURATION_SUFFIX /property:InteropCodec=false /property:InteropLog=false /property:CheckState=true /property:CountHandle=true /property:TraceConnection=true /property:TraceDetection=true /property:TraceHandle=true /property:TraceStatement=true /property:TrackMemoryBytes=true "$@"
+popd
--- Setup/build-netstandard20-debug.sh
+++ Setup/build-netstandard20-debug.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 scriptdir=`dirname "$BASH_SOURCE"`
+
+if [[ -z "$SQLITE_NET_YEAR" ]]; then
+  SQLITE_NET_YEAR=NetStandard20
+fi
+
 pushd "$scriptdir/.."
-
-dotnet build SQLite.NET.NetStandard20.MSBuild.sln /property:Configuration=Debug /property:ConfigurationSuffix=$SQLITE_NET_CONFIGURATION_SUFFIX /property:InteropCodec=false /property:InteropLog=false /property:CheckState=true /property:CountHandle=true /property:TraceConnection=true /property:TraceDetection=true /property:TraceHandle=true /property:TraceStatement=true /property:TrackMemoryBytes=true "$@"
-
+dotnet build SQLite.NET.$SQLITE_NET_YEAR.MSBuild.sln /property:Configuration=Debug /property:ConfigurationSuffix=$SQLITE_NET_CONFIGURATION_SUFFIX /property:InteropCodec=false /property:InteropLog=false /property:CheckState=true /property:CountHandle=true /property:TraceConnection=true /property:TraceDetection=true /property:TraceHandle=true /property:TraceStatement=true /property:TrackMemoryBytes=true "$@"
 popd
