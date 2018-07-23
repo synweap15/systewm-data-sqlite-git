194cd86ab28a3f55a868c58dbd0155db9627912e
mistachkin <mistachkin>
2018-07-23 05:52:06 +0000

    Improve cross-platform detection and use of the SQLite interop assembly / SQLite native library by the test suite infrastructure.

--- Setup/run-netstandard20-tests-release.sh
+++ Setup/run-netstandard20-tests-release.sh
@@ -19,10 +19,9 @@ SQLITE_INTEROP_FILE=$SQLITE_INTEROP_DIR/$libname
 
 if [[ -f "${SQLITE_INTEROP_FILE}" ]]; then
   cp "$SQLITE_INTEROP_FILE" "$SQLITE_INTEROP_DIR/SQLite.Interop.dll"
+  libname=SQLite.Interop.dll
 fi
 
-libname=SQLite.Interop.dll
-
-dotnet exec Externals/Eagle/bin/netStandard20/EagleShell.dll -preInitialize "set test_configuration Release; set test_year NetStandard20; set test_native_year $SQLITE_NET_YEAR; set interop_assembly_file_names $libname" -initialize -postInitialize "unset no(deleteSqliteImplicitNativeFiles); unset no(copySqliteImplicitNativeFiles)" -file Tests/all.eagle "$@"
+dotnet exec Externals/Eagle/bin/netStandard20/EagleShell.dll -preInitialize "set test_configuration Release; set test_year NetStandard20; set test_native_year $SQLITE_NET_YEAR" -file Tests/all.eagle "$@"
 
 popd
