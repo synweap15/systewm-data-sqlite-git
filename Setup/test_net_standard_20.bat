8037340061cc2554dab2dba88bc1852358790caf
mistachkin <mistachkin>
2018-08-13 19:47:30 +0000

    Make it possible for the managed and native test configuration suffixes to differ.

--- Setup/test_net_standard_20.bat
+++ Setup/test_net_standard_20.bat
@@ -181,10 +181,9 @@ GOTO no_errors
   )
   SET CONFIGURATION=%NATIVE_CONFIGURATION%
   SET CONFIGURATION=%CONFIGURATION:NativeOnly=%
-  SET CONFIGURATION=%CONFIGURATION%NetStandard20
-  IF EXIST "bin\%YEAR%\%CONFIGURATION%\bin" (
+  IF EXIST "bin\%YEAR%\%CONFIGURATION%NetStandard20\bin" (
     IF EXIST "bin\%NATIVE_YEAR%\%PLATFORM%\%NATIVE_CONFIGURATION%" (
-      %__ECHO% "%DOTNET%" %SUBCOMMANDS% "Externals\Eagle\bin\netStandard20\%EAGLESHELL%" %PREARGS% -anyInitialize "set test_year {%YEAR%}; set test_native_year {%NATIVE_YEAR%}; set test_configuration {%CONFIGURATION%}" -file "%TEST_FILE%" %POSTARGS%
+      %__ECHO% "%DOTNET%" %SUBCOMMANDS% "Externals\Eagle\bin\netStandard20\%EAGLESHELL%" %PREARGS% -anyInitialize "set test_year {%YEAR%}; set test_native_year {%NATIVE_YEAR%}; set test_configuration {%CONFIGURATION%}; set test_configuration_suffix NetStandard20" -file "%TEST_FILE%" %POSTARGS%
       IF ERRORLEVEL 1 (
         ECHO Testing of "%YEAR%/%NATIVE_YEAR%/%CONFIGURATION%" .NET Standard 2.0 assembly failed.
         CALL :fn_SetErrorLevel
@@ -194,7 +193,7 @@ GOTO no_errors
       %_AECHO% Native directory "bin\%NATIVE_YEAR%\%PLATFORM%\%NATIVE_CONFIGURATION%" not found, skipped.
     )
   ) ELSE (
-    %_AECHO% Managed directory "bin\%YEAR%\%CONFIGURATION%\bin" not found, skipped.
+    %_AECHO% Managed directory "bin\%YEAR%\%CONFIGURATION%NetStandard20\bin" not found, skipped.
   )
   GOTO :EOF
 
