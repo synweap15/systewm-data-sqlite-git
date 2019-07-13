88041dc04f4bf4237eb08fa8add757b476df8c37
mistachkin <mistachkin>
2019-07-13 19:11:43 +0000

    Bump version to 1.0.112.0.

--- testce/AssemblyInfo.cs
+++ testce/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.111.0")]
-// [assembly: AssemblyFileVersion("1.0.111.0")]
+[assembly: AssemblyVersion("1.0.112.0")]
+// [assembly: AssemblyFileVersion("1.0.112.0")]
